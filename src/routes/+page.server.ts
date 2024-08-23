363d08ef3c11ae8a91710b68083becc0712eb959
hathwars <44913507+hathwars@users.noreply.github.com>
2024-08-23 15:21:53 -0700

    Update +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,7 +5,7 @@ import { BLOB_READ_WRITE_TOKEN } from '$env/static/private'
 export const actions = {
   upload: async ({ request }) => {
     const form = await request.formData()
-    const file = form.get('image-upload') as File
+    const file = form.get('file-upload') as File
 
     if (!file) {
       error(400, { message: 'No file to upload.' })
