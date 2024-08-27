68de05bcb2a30e14d19ae4479e1b01ace88c4aa0
Sriram Hathwar <shathwar@princeton.edu>
2024-08-26 17:24:17 -0700

    simplified page.svelte

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,24 +1,20 @@
-import { error } from '@sveltejs/kit'
-import { put } from '@vercel/blob'
-import { BLOB_READ_WRITE_TOKEN } from '$env/static/private'
+// import { error } from '@sveltejs/kit'
+// import { put } from '@vercel/blob'
+// import { BLOB_READ_WRITE_TOKEN } from '$env/static/private'
 
-export const actions = {
-  upload: async ({ request }) => {
-    const form = await request.formData()
-    const file = form.get('pdf-upload') as File
+// export const actions = {
+//   upload: async ({ request }) => {
+//     const form = await request.formData()
+//     const file = form.get('pdf-upload') as File
 
-    console.log("Form:", form);
-    console.log(`File size: ${file.size} bytes`);
-    console.log("File:", file);
-    
-    if (!file) {
-      error(400, { message: 'No file to upload.' })
-    }
+//     if (!file) {
+//       error(400, { message: 'No file to upload.' })
+//     }
 
-    const { url } = await put(file.name, file, {
-      access: 'public',
-      token: BLOB_READ_WRITE_TOKEN,
-    })
-    return { uploaded: url }
-  },
-}
+//     const { url } = await put(file.name, file, {
+//       access: 'public',
+//       token: BLOB_READ_WRITE_TOKEN,
+//     })
+//     return { uploaded: url }
+//   },
+// }
